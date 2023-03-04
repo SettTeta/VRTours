f7a2986ba904ac5ae16616264f9d37721cc659de
Sett Teta <stetanonsakul@gmail.com>
2023-03-04 20:40:30 +0700

    added video player

--- pages/story/index.js
+++ pages/story/index.js
@@ -7,6 +7,7 @@ import Script from "next/script";
 export default function ImmersionZone() {
     const [fr, setFr] = useState(false);
     const [playing, setPlaying] = useState(false);
+    const [videoPlayer, setVideoPlayer] = useState(true);
     const [videoIndex, setVideoIndex] = useState(0);
 
     const videoOrderList = ["#intro", "#sala", "#end"];
@@ -62,12 +63,15 @@ export default function ImmersionZone() {
         setPlaying(false);
     };
 
-
-
-
+    const handleHide = () => {
+        if (videoPlayer === false) {
+            setVideoPlayer(true)
+        } else {
+            setVideoPlayer(false)
+        }
+    }
 
     console.log("playing:", playing);
-
 
     return (
         <>
@@ -80,77 +84,114 @@ export default function ImmersionZone() {
                 {fr && (
 
                     <div>
+                        {/* aframe-gui-component */}
+                        <Script src="https://rawgit.com/rdub80/aframe-gui/master/dist/aframe-gui.min.js" />
 
-                        {/* <Script src="https://rawgit.com/rdub80/aframe-gui/master/dist/aframe-gui.min.js" /> */}
 
                         <button onClick={() => document.querySelector('Scene').requestFullscreen()} style={{ paddingTop: "70px" }}></button>
 
                         <Scene>
                             <a-assets>
+                                <audio
+                                    id="click-sound"
+                                    src="https://cdn.aframe.io/360-image-gallery-boilerplate/audio/click.ogg"
+                                ></audio>
+
                                 <video
                                     id="intro"
                                     src="intro.mp4"
                                     loop
+                                    autoPlay
                                     playsInline
                                 ></video>
                                 <video
                                     id="sala"
                                     src="sala-jat.mp4"
                                     loop
+                                    autoPlay
                                     playsInline
                                 ></video>
                                 <video
                                     id="end"
                                     src="end.mp4"
                                     loop
+                                    autoPlay
                                     playsInline
                                 ></video>
                             </a-assets>
 
                             <a-camera>
-                                <a-cursor ></a-cursor>
+                                <a-cursor
+                                    id="cursor"
+                                    animation__click="property: scale; from: 0.1 0.1 0.1; to: 1 1 1; easing: easeInCubic; dur: 150; startEvents: click"
+                                    // animation__clickreset="property: scale; to: 0.1 0.1 0.1; dur: 1; startEvents: animationcomplete__click"
+                                    animation__fusing="property: scale; from: 1 1 1; to: 0.1 0.1 0.1; easing: easeInCubic; dur: 150; startEvents: fusing"></a-cursor>
                             </a-camera>
+
 
                             <a-videosphere
                                 id="videosphere"
                                 src={currentVideo}
                                 rotation="0 -90 0"
-                                // onClick={handlePlay}
-                                autoPlay
+                                // autoPlay
                                 playsInline
                             ></a-videosphere>
 
-                            {playing && (
-                                <Entity
-                                    position="0 1 -1"
-                                    // rotation="-15 20 0"
-                                    pause-icon="size: 0.3; color: #ffffff"
-                                    // onClick={handlePause}
-                                    events={{ click: handlePause, touchStart: handlePause }}
-                                ></Entity>
-                            )}
+                            {!videoPlayer && (<Entity id="show"
+                                position="0 1 -1"
+                                geometry="primitive: box; width: 0.2; height: 0.05; depth:0.1;"
+                                material="color: #ffffff"
+                                events={{ click: handleHide, touchStart: handleHide }}
+                            />)}
 
-                            {!playing && (
-                                <Entity
-                                    position="0 1 -1"
-                                    // rotation="-15 -20 0"
-                                    play-icon="size: 1; color: #ffffff"
-                                    // onClick={handlePlay}
-                                    events={{ click: handlePlay, touchStart: handlePlay }}
-                                ></Entity>
-                            )}
-
-                            <Entity
-                                position="0.5 1 -1"
-                                events={{ click: handleVideoChange }}
-                                play-icon="size: 1; color: white"
-                                />
-                            <Entity
-                                position="0.7 1 -1"
-                                events={{ click: handleVideoChange }}
-                                play-icon="size: 1; color: white"
+                            {videoPlayer && (<Entity
+                                position="0 1 -1"
+                                rotation="-15 0 0"
+                                geometry="primitive: plane; width: 2; height: 0.8"
+                                material="color: #ffffff; opacity: 0.5"
+                            >
+                                <Entity id="title"
+                                    position="0 0.3 0"
+                                    text="value: Video Player; align: center; color: #000000"
                                 />
 
+                                <Entity id="hide"
+                                    position="0.8 0.3 0"
+                                    geometry="primitive: box; width: 0.2; height: 0.05; depth:0.1;"
+                                    material="color: #ffffff"
+                                    events={{ click: handleHide, touchStart: handleHide }}
+                                />
+
+                                {playing && (
+                                    <Entity id="pause"
+                                        // position="0 1 -1"
+                                        pause-icon="size: 0.3; color: #ffffff"
+                                        events={{ click: handlePause, touchStart: handlePause }}
+                                    ></Entity>
+                                )}
+
+                                {!playing && (
+                                    <Entity id="play"
+                                        // position="0 1 -1"
+                                        play-icon="size: 1; color: #ffffff"
+                                        events={{ click: handlePlay, touchStart: handlePlay }}
+                                        sound="on: click; src: #click-sound"
+                                    ></Entity>
+                                )}
+
+                                <Entity id="next" events={{ click: handleVideoChange }} sound="on: click; src: #click-sound">
+                                    <Entity
+                                        position="0.5 0 0"
+                                        play-icon="size: 1; color: white"
+                                    />
+                                    <Entity
+                                        position="0.7 0 0"
+                                        play-icon="size: 1; color: white"
+                                    />
+                                </Entity>
+
+                            </Entity>
+                            )}
 
                         </Scene>
                     </div>
--- pages/test2/index.js
+++ pages/test2/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Scene, Entity } from "aframe-react";
 import Head from 'next/head'
 import Script from "next/script";
@@ -7,6 +7,13 @@ import Script from "next/script";
 export default function ImmersionZone() {
     const [fr, setFr] = useState(false);
     const [playing, setPlaying] = useState(false);
+    const [videoIndex, setVideoIndex] = useState(0);
+
+    const videoOrderList = ["#intro", "#sala", "#end"];
+
+    const [currentVideo, setCurrentVideo] = useState(videoOrderList[videoIndex]);
+
+
 
     useEffect(() => {
         const loadAframe = async () => {
@@ -21,18 +28,43 @@ export default function ImmersionZone() {
 
     const handlePlay = () => {
         if (!playing) {
+            const video = document.querySelector(currentVideo);
+            video.play();
             setPlaying(true);
         }
-        console.log("playing")
     };
 
     const handlePause = () => {
         if (playing) {
+            const video = document.querySelector(currentVideo);
+            video.pause();
+            setPlaying(false);
+        }
+    };
+
+    const handleVideoChange = () => {
+        const i = videoIndex + 1;
+        if (i >= videoOrderList.length) {
+            setVideoIndex(0);
+        } else {
+            setVideoIndex(i)
+        }
+
+        // Pause the currently playing video (if any)
+        if (playing) {
+            const currentVideoRef = document.querySelector(currentVideo);
+            currentVideoRef.pause();
             setPlaying(false);
         }
 
-        console.log("pausing")
+        // Set the new video source and reset the playing state
+        setCurrentVideo(videoOrderList[videoIndex]);
+        setPlaying(false);
     };
+
+
+
+
 
     console.log("playing:", playing);
 
@@ -48,60 +80,97 @@ export default function ImmersionZone() {
                 {fr && (
 
                     <div>
-                        
-                        {/* <Script src="https://rawgit.com/rdub80/aframe-gui/master/dist/aframe-gui.min.js" /> */}
+                        {/* aframe-gui-component */}
+                        <Script src="https://rawgit.com/rdub80/aframe-gui/master/dist/aframe-gui.min.js" />
+
+                        {/* for layout */}
+                        <Script src="https://unpkg.com/aframe-layout-component@4.x.x/dist/aframe-layout-component.min.js"></Script>
 
                         <button onClick={() => document.querySelector('Scene').requestFullscreen()} style={{ paddingTop: "70px" }}></button>
 
                         <Scene>
                             <a-assets>
+                                <audio
+                                    id="click-sound"
+                                    src="https://cdn.aframe.io/360-image-gallery-boilerplate/audio/click.ogg"
+                                ></audio>
+
                                 <video
                                     id="intro"
                                     src="intro.mp4"
                                     loop
+                                    autoPlay
                                     playsInline
                                 ></video>
                                 <video
                                     id="sala"
                                     src="sala-jat.mp4"
                                     loop
+                                    autoPlay
                                     playsInline
                                 ></video>
                                 <video
                                     id="end"
                                     src="end.mp4"
                                     loop
+                                    autoPlay
                                     playsInline
                                 ></video>
                             </a-assets>
 
                             <a-camera>
-                                <a-cursor ></a-cursor>
+                                <a-cursor
+                                    id="cursor"
+                                    animation__click="property: scale; from: 0.1 0.1 0.1; to: 1 1 1; easing: easeInCubic; dur: 150; startEvents: click"
+                                    // animation__clickreset="property: scale; to: 0.1 0.1 0.1; dur: 1; startEvents: animationcomplete__click"
+                                    animation__fusing="property: scale; from: 1 1 1; to: 0.1 0.1 0.1; easing: easeInCubic; dur: 150; startEvents: fusing"></a-cursor>
                             </a-camera>
 
                             <a-videosphere
                                 id="videosphere"
-                                src={playing ? "#sala" : "#intro"}
+                                src={currentVideo}
                                 rotation="0 -90 0"
-                                autoPlay
+                                // onClick={handlePlay}
                                 playsInline
                             ></a-videosphere>
 
-                            {playing && (
+
+                            <Entity id="gui" layout="type: line; margin: 1" position="0 1 -1">
+                                {playing && (
                                 <Entity
-                                    position="-0.3 1 -1"
+                                    // position="0 1 -1"
+                                    // rotation="-15 20 0"
                                     pause-icon="size: 0.3; color: #ffffff"
+                                    // onClick={handlePause}
                                     events={{ click: handlePause, touchStart: handlePause }}
                                 ></Entity>
-                            )}
+                              )} 
 
-                            {!playing && (
+                                 {!playing && ( 
                                 <Entity
-                                    position="0.3 1 -1"
+                                    // position="0 1 -1"
+                                    // rotation="-15 -20 0"
                                     play-icon="size: 1; color: #ffffff"
+                                    // onClick={handlePlay}
                                     events={{ click: handlePlay, touchStart: handlePlay }}
+                                    sound="on: click; src: #click-sound"
                                 ></Entity>
-                            )}
+                              )} 
+
+                                <Entity
+                                    // position="0.5 1 -1"
+                                    events={{ click: handleVideoChange }}
+                                    play-icon="size: 1; color: white"
+                                    sound="on: click; src: #click-sound"
+                                />
+                                <Entity
+                                    // position="0.7 1 -1"
+                                    events={{ click: handleVideoChange }}
+                                    play-icon="size: 1; color: white"
+                                    sound="on: click; src: #click-sound"
+                                />
+
+                            </Entity>
 
                         </Scene>
                     </div>
